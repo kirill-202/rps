a88d0c94e1521fce1ac1e7f9e6c521a1b13922e8
Kirill <kirillgoodline@gmail.com>
2025-01-26 19:59:14 +0400

    add playGame playRound

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
-const OPTIONS = ["Rock", "Paper", "Scissors"];
-
+const OPTIONS = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let index = Math.floor(Math.random()* OPTIONS.length);
@@ -10,7 +9,8 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    const pmt = prompt("Please provide your choice for the current round (Rock, Paper, Scissors)");
+    let pmt = prompt("Please provide your choice for the current round (Rock, Paper, Scissors)");
+    pmt =  pmt.toLowerCase();
     console.log(`user input is ${pmt}`);
 
     if (OPTIONS.includes(pmt)) {
@@ -21,11 +21,46 @@ function getHumanChoice() {
     return getHumanChoice();
     
 }
-console.log(getHumanChoice());
 
 
-function play() {
-    let playerScore = 0;
-    let computerScore = 0;
+function playRound() {
+    const cChoice = getComputerChoice();
+    const hChoice = getHumanChoice();
+
+    if (cChoice === hChoice) {
+        console.log("Draw. No points added")
+    } else if (
+        (cChoice === "rock" && hChoice === "paper") ||
+        (cChoice === "paper" && hChoice === "scissors") ||
+        (cChoice === "scissors" && hChoice === "rock")
+    ) {
+        console.log("Player won!");
+        console.log(`Player: ${hChoice}\nComputer: ${cChoice}`);
+        playerScore +=1;
+    } else {
+        console.log("Coputer won!");
+        console.log(`Player: ${hChoice}\nComputer: ${cChoice}`);
+        computerScore +=1;
+    }
+    console.log("Round has finished.");
+    return;
 
 }
+
+
+function playGame() {
+    const maxScore = 5;
+    while (playerScore < maxScore && computerScore < maxScore) {
+        playRound();
+        console.log(`Current score - Computer: ${computerScore} - Player: ${playerScore}`);
+    }
+    
+    if (playerScore === maxScore) {
+        console.log("Player wins the game!");
+    } else {
+        console.log("Computer wins the game!");
+    }
+    return;
+}
+
+playGame();
