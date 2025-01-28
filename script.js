fa7a6d1114373aae0cf1ae024ac11a8ee733fb5e
Kirill <kirillgoodline@gmail.com>
2025-01-29 02:45:57 +0400

    add ui and styles

--- script.js
+++ script.js
@@ -2,65 +2,94 @@ const OPTIONS = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
+const counter = document.createElement("div");
+counter.className = "counter";
+counter.textContent = `Player score = ${playerScore} ----- Computer score = ${computerScore}`;
+document.body.appendChild(counter);
+
 function getComputerChoice() {
     let index = Math.floor(Math.random()* OPTIONS.length);
     return OPTIONS.at(index);
 }
 
 
-function getHumanChoice() {
-    let pmt = prompt("Please provide your choice for the current round (Rock, Paper, Scissors)");
-    pmt =  pmt.toLowerCase();
-    console.log(`user input is ${pmt}`);
-
-    if (OPTIONS.includes(pmt)) {
-        return pmt;
-    }
-
-    console.log("User input is not one of legal options. Try again.");
-    return getHumanChoice();
-    
-}
-
-
-function playRound() {
+function playRound(hChoice) {
     const cChoice = getComputerChoice();
-    const hChoice = getHumanChoice();
+
 
     if (cChoice === hChoice) {
-        console.log("Draw. No points added")
+        alert("Draw. No points added");
     } else if (
         (cChoice === "rock" && hChoice === "paper") ||
         (cChoice === "paper" && hChoice === "scissors") ||
         (cChoice === "scissors" && hChoice === "rock")
     ) {
-        console.log("Player won!");
-        console.log(`Player: ${hChoice}\nComputer: ${cChoice}`);
+        alert("Player won this round!");
         playerScore +=1;
     } else {
-        console.log("Coputer won!");
-        console.log(`Player: ${hChoice}\nComputer: ${cChoice}`);
+        alert("Computer won this round!");
+
         computerScore +=1;
     }
-    console.log("Round has finished.");
+    counter.textContent = `Player score = ${playerScore} ----- Computer score = ${computerScore}`;
     return;
 
 }
 
+function initButtons() {
+    const container = document.createElement("div");
+    container.className = "container";
+    container.style.display = "flex"; 
+    container.style.justifyContent = "space-evenly"; 
+    container.style.alignItems = "center"; 
+    container.style.padding = "20px";
 
+    const scissors = document.createElement("button");
+    scissors.value = "scissors";
+    scissors.className = "button";
+    scissors.textContent = "Scissors";
+
+    const paper = document.createElement("button");
+    paper.className = "button";
+    paper.value = "paper";
+    paper.textContent = "Paper";
+
+    const rock = document.createElement("button");
+    rock.className = "button";
+    rock.value = "rock";
+    rock.textContent = "Rock";
+
+    container.appendChild(scissors);
+    container.appendChild(paper);
+    container.appendChild(rock);
+    document.body.insertBefore(container, document.body.firstChild);
+    return container;
+}
+
+function resetScore() {
+    let playerScore = 0;
+    let computerScore = 0;
+    counter.textContent = `Player score = ${playerScore} ----- Computer score = ${computerScore}`;
+}
 function playGame() {
-    const maxScore = 5;
-    while (playerScore < maxScore && computerScore < maxScore) {
-        playRound();
-        console.log(`Current score - Computer: ${computerScore} - Player: ${playerScore}`);
-    }
+
+    const container = initButtons();
+
+    container.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            playRound(event.target.value);
+        }
+
+        if (playerScore === 5) {
+            alert("Player won the game!!!");
+            resetScore();
+        } else if (computerScore === 5) {
+            alert("Computer won the game!!!");
+            resetScore();
+        }
+    });
+
     
-    if (playerScore === maxScore) {
-        console.log("Player wins the game!");
-    } else {
-        console.log("Computer wins the game!");
-    }
-    return;
 }
 
 playGame();
